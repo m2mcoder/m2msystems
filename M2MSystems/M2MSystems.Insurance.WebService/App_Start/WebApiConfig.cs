a1ce939f3b2a174d0f475d00d4911231378144e3
m2mcoder <vladimir.moushkov@gmail.com>
2014-02-21 18:56:54 +0200

    Outline the projects

---
+++ M2MSystems/M2MSystems.Insurance.WebService/App_Start/WebApiConfig.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Http;
+
+namespace M2MSystems.Insurance.WebService
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            //name: "DefaultApi",
+            //routeTemplate: "api/{controller}/{id}",
+            //defaults: new { id = RouteParameter.Optional }
+            config.Routes.MapHttpRoute(
+                "InsuranceGet",
+                "api/1/insurance/{partnerId}/{formId}",
+                new { controller = "InsuranceGet" }
+            );
+            config.Routes.MapHttpRoute(
+                "InsuranceQuote",
+                "api/1/insurance/{partnerId}/{formId}/quote",
+                new { controller = "InsuranceQuote" }
+            );
+        }
+    }
+}
---
+++ M2MSystems/M2MSystems.Insurance.WebService/Controllers/InsuranceGetController.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Web.Http;
+
+namespace M2MSystems.Insurance.WebService.Controllers
+{
+    public class InsuranceGetController : ApiController
+    {
+        public HttpResponseMessage Get(long partnerId, long formId)
+        {
+            var result = new HttpResponseMessage(HttpStatusCode.OK);
+            var stream = new MemoryStream(
+                Encoding.UTF8.GetBytes(
+                string.Format(
+                    @"<html><body>Hello, {0} partner!<br/> This is form with id {1}!</body></html", 
+                        partnerId, formId)));
+            result.Content = new StreamContent(stream);
+            result.Content.Headers.ContentType = new MediaTypeHeaderValue("text/html");
+            return result;
+        }
+    }
+}
